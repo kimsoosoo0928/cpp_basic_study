e51d51baf4231db033633e4833e18f6703855501
kimsoosoo0928 <kimsoosoo0928@gmail.com>
2021-12-09 22:24:37 +0900

    study

--- cpp_study/ConsoleApplication1/cpp_study_02.cpp
+++ cpp_study/ConsoleApplication1/cpp_study_02.cpp
@@ -1,11 +1,14 @@
-﻿/*
+﻿// 참조자의 도입
+
+/*
 #include <iostream>
 
-int change_val(int *p) {
-	*p = 3;
+int change_val(int *p) { // change 함수의 인자 p에 number의 주소값을 전달
+	*p = 3; // *p 를 통해 number를 참조하여 number의 값을 3으로 변경 
 
 	return 0;
 }
+
 int main() {
 	int number = 5;
 
@@ -15,17 +18,168 @@ int main() {
 }
 */
 
-# include <iostream>
+/*
+#include <iostream>
 
 int main() {
 	int a = 3;
-	int & another_a = a;
-
+	int& another_a =  a;
 	another_a = 5;
-
-	std::cout << "a : " << a << std::endl;
-	std::cout << "another_a : " << another_a << std::endl;
+	std::cout << "a :" << a << std::endl;
+	std::cout << "another_a :" << another_a << std::endl;
 
 	return 0;
 }
+*/
+
+/*
+#include <iostream>
+
+int chage_val(int &p) {
+	p = 3;
+
+	return 0;
+}
+int main() {
+	int number = 5;
+
+	std::cout << number << std::endl;
+	change_val(number);
+	std::cout << number << std::endl;
+}
+*/
+
+// change_val 안에서 p = 3; 이라 하는 것은 main 함수의 number에 number = 3;
+
+// 참조자 이해하기
+/*
+#include <iostream>
+
+int main() {
+	int x;
+	int& y = x;
+	int& z = y;
+
+	x = 1;
+	std::cout << "x : " << " y : " << y << " z : " << z << std::endl;
+
+	y = 2;
+	std::cout << "x : " << x << " y : " << y << " z : " << z << std::endl;
+	
+	z = 3;
+	std::cout << "x : " << x << " y : " << y << " z : " << z << std::endl;
+}
+*/
+
+// 상수에 대한 참조자
+/*
+#include <iostream>
+int main() {
+	int &ref = 4;
+	const int &ref = 4;
+	std::cout << ref << std::endl;
+}
+*/
+/*
+// 레퍼런스의 배열과 배열의 레퍼런스
+int a, b;
+int& arr[2] = { a,b };
+// 레퍼런스의 레퍼런스, 레페런스의 배열, 레퍼런스의 포인터는 존재할 수 없다.
+*/
+/*
+#include <iostream>
+
+int main() {
+	int arr[3] = { 1,2,3 };
+	int(&ref)[3] = arr; // ref가 arr를 참조하도로고 함
+
+	ref[0] = 2;
+	ref[1] = 3;
+	ref[2] = 1;
+
+	std::cout << arr[0] << arr[1] << arr[2] << std::endl;
+	return 0;
+}
+*/
+/*
+int function() {
+	int a = 2;
+	return a;
+}
+
+int main() {
+	int b = function(); // function 안에 정의된 a라는 변수의 값이 b에 복사되었습니다. 여기서 주목할 점은 복사 되었다는 점입니다. 
+	return 0; 
+}
+*/
+
+/*
+int& function() { // 참조자를 리턴
+	int a = 2;
+	return a; // a는 함수의 리턴과 함께 사라진다. 
+}
+
+int main() {
+	int b = function(); // 본체는 사라지고 별명만 남아 있는 상황 
+	b = 3;
+	return 0;
+}
+
+// 레퍼런스를 리턴하는 함수에서 지역 변수의 레퍼런스를 리턴하지 않도록 조심해야 합니다.
+
+*/
+
+// 외부 변수의 레퍼런스를 리턴 
+/*
+int& function(int& a) {
+	a = 5;
+	return a; // 레퍼런스 리턴 
+	// 위와 같이 인자로 받은 레퍼런스를 그대로 리턴 
+	
+}
+
+int main() {
+	int b = 2;
+	int c = function(b);
+	return 0;
+}
+
+// 전체 복사 VS 포인터 주소 복사
+
+*/
+
+// 참조자가 아닌 값을 리턴하는 함수를 참조자로 받기
+
+/*
+int function() {
+	int a = 5;
+	return a; // 리턴값은 바로 사라진다.
+}
+
+int main() {
+	int& c = function();
+	return 0;
+	
+}
+
+*/
+
+#include <iostream>
+
+int function() {
+	int a = 5;
+	return a;
+}
+
+
+int main() {
+	const int& c = function();
+	std::cout << "c : " << c << std::endl;
+	return 0;
+
+	// 상수 레퍼런스로 리턴값을 받게 되면 해당 리턴값의 생명이 연장된다.
+
+}
+
+
 
---
+++ cpp_study_main/cpp_study_main/cpp_study_main.cpp
@@ -0,0 +1,39 @@
+﻿/*
+#include <iostream>
+
+int main() {
+	int* p = new int; // 할당
+	*p = 10;
+
+	std::cout << *p << std::endl;
+
+	delete p; // 할당 해제
+	return 0;
+}*/
+
+/*
+#include <iostream>
+
+int main() {
+	int a = 5;
+	delete& a;
+	return 0;
+}
+*/
+
+#include <iostream>
+
+int main() {
+	int arr_size; // 배열의 크기를 잡을 변수 정의
+	std::cout << "array size : ";
+	std::cin >> arr_size; // 값을 입력 받음 
+	int* list = new int[arr_size]; // list에 new를 이용하여 크기가 arr_size인 int 배열을 생성 
+	for (int i = 0; i < arr_size; i++) {
+		std::cin >> list[i];
+	}
+	for (int i = 0; i< arr_size; i++) {
+		std::cout << i << "th element of list : " << list[i] << std::endl;
+	}
+	delete[] list;
+	return 0;
+}
